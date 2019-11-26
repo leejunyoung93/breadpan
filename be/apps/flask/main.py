e5225f517b89d5ce48355993f628cb4c2c2532d6
JinhoYoo <jinho.yoo@theloop.co.kr>
2019-11-26 14:19:22 +0900

    Implemented basic RESTful API server.

--- be/apps/flask/main.py
+++ be/apps/flask/main.py
@@ -1,10 +1,17 @@
 from flask import Flask
-from flask_restful import reqparse, abort, Api, Resource
-
+from flask_restful import Api
+from be.apps.flask import resources as r
 
 app = Flask(__name__)
 api = Api(app)
 
 
+##
+# Actually setup the Api resource routing here
+##
+
+api.add_resource(r.FlaskTodoListController, '/todos')
+api.add_resource(r.FlaskTodoController, '/todos/<todo_id>')
+
 if __name__ == "__main__":
-    print("Hello")
+    app.run(debug=True)
--- be/apps/flask/resources/__init__.py
+++ be/apps/flask/resources/__init__.py
@@ -6,20 +6,39 @@
 """
 
 from http import HTTPStatus
+from flask_restful import reqparse, abort, Api, Resource
 
-# Todo
-# shows a single todo item and lets you delete a todo item
+TODOS = {
+    'todo1': {'task': 'build an API'},
+    'todo2': {'task': '?????'},
+    'todo3': {'task': 'profit!'},
+}
+
+
+def abort_if_todo_doesnt_exist(todo_id):
+    if todo_id not in TODOS:
+        abort(404, message="Todo {} doesn't exist".format(todo_id))
+
+
+parser = reqparse.RequestParser()
+parser.add_argument('task')
 
 
 class FlaskTodoController(Resource):
+
     def get(self, todo_id):
         abort_if_todo_doesnt_exist(todo_id)
         return TODOS[todo_id], HTTPStatus.OK
 
     def delete(self, todo_id):
+        abort_if_todo_doesnt_exist(todo_id)
+        del TODOS[todo_id]
         return '',  HTTPStatus.NO_CONTENT
 
     def put(self, todo_id):
+        args = parser.parse_args()
+        task = {'task': args['task']}
+        TODOS[todo_id] = task
         return task, HTTPStatus.CREATED
 
 
