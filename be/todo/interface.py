b3a9d66a36e150612d1d4e756b6031909829638d
Jinho Yoo <jinho.yoo@theloop.co.kr>
2019-11-29 11:42:27 +0900

    Applied presenter object.

--- be/todo/interface.py
+++ be/todo/interface.py
@@ -1,29 +1,26 @@
-from breadpan.interface import IController
+from breadpan.interface import IController, IPresenter
 from breadpan.usecase import IUsecaseOutputPort, IUsecaseInteractor
 from todo.usecase import ToDoCreateInteractor, ToDoReadInteractor, ToDoDeleteInteractor
 
 
-class ToDoPresenter(IUsecaseOutputPort):
-    def __init__(self, **kwargs):
-        super(IUsecaseOutputPort,self).__init__(**kwargs)
-        # To-Do: Do any operation additionally.
-
+class ToDoPresenter(IPresenter):
+    pass
 
 class ToDoController(IController):
     def create(self, todo_id, contents):
         i = ToDoCreateInteractor()
         i.input(todo_id=todo_id, contents=contents)
-        return i.operate()
+        return ToDoPresenter(i.operate())
 
     def read(self, todo_id):
         i = ToDoReadInteractor()
         i.input(todo_id=todo_id)
-        return i.operate()
+        return ToDoPresenter(i.operate())
 
     def delete(self, todo_id):
         i = ToDoDeleteInteractor()
         i.input(todo_id=todo_id)
-        return i.operate()
+        return ToDoPresenter(i.operate())
 
     def update(self,  **kwargs):
         pass
